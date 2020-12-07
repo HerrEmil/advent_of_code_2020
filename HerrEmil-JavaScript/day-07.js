7ecf501fa61c0ec253886e4c21266b04d9ce8370
Emil Andersson <emil.andersson@neotechnology.com>
2020-12-07 08:00:18 +0100

    day 7 WIP

--- HerrEmil-JavaScript/day-07.js
+++ HerrEmil-JavaScript/day-07.js
@@ -1,11 +1,69 @@
-const parseData = (data) => data.split(/\r?\n/);
+const parseData = (data) =>
+  data.split(/\r?\n/).map((bag) => {
+    const [color, contents] = bag.split(" bags contain ");
+    let containsList = contents
+      .replace(".", "")
+      .split(", ")
+      .map((bag) => bag.replace(/ bags?/, ""))
+      .map((bag) => ({
+        [bag.substring(2)]: parseInt(bag.charAt(0), 10),
+      }));
 
-const solve1 = (data) => {
-return data
-}
+    if (bag.includes("no other bags.")) {
+      containsList = [];
+    }
+    const parsedBag = {
+      [color]: containsList.reduce(
+        (prev, curr) => Object.assign(prev, curr),
+        {}
+      ),
+    };
 
-const solve2 = (data) => {
-return data
+    return parsedBag;
+  });
+
+const canHoldColor = (rules, rule, targetColor) => {
+  const contents = Object.values(rule)[0];
+  // if rule contains targetColor, return true
+  if (contents.hasOwnProperty(targetColor)) {
+    return true;
+  }
+  // for each contains, find rule and check if it can hold color
+  return !!Object.keys(contents).find((color) => {
+    const childRule = rules.find((rule) => rule.hasOwnProperty(color));
+    return canHoldColor(rules, childRule, targetColor);
+  });
+};
+
+const solve1 = (bagRules) => {
+  const canIncludeShinyGold = bagRules.filter((rule, index, rules) =>
+    canHoldColor(rules, rule, "shiny gold")
+  );
+  return canIncludeShinyGold.length;
+};
+
+const bagCount = (bag, rules) => {
+  const children = Object.entries(Object.values(bag)[0]);
+  // if no children, return 1
+  if (children.length === 0) {
+    return 1;
+  }
+  console.log(children)
+  // for each of it children, return number * recursive
+  return children.map(([color, count]) => {
+    // find rule by color
+    const child = rules.find((rule) => rule[color]);
+    console.log(child)
+    return  (count * bagCount(child, rules))
+  }).reduce((acc, curr) => acc + curr, 0)
+};
+
+const solve2 = (rules) => {
+  // find shiny gold bag
+  const shinyGold = rules.find((rule) => rule["shiny gold"]);
+  console.log(shinyGold);
+
+  return bagCount(shinyGold, rules);
 };
 
 module.exports.solve1 = solve1;
